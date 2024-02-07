8b3aef4ee793474e5090e8fa6f5a3a8ec9b6c4f2
Shrinath <shrinathpatil1204@gmail.com>
2024-02-07 12:40:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,jsonify
+
+
+app=Flask(__name__)
+
+print("hello")
+
+@app.route("/api/test", methods=['GET'])
+def message():
+  body=request.get_json()
+  print(body)
+  return "hello"
+
+@app.route("/api/hello/<string:name>", methods=['GET'])
+def sayHello(name):
+  print(name)
+  return jsonify({"data":f"Hello, {name}","code":69}),200
+
+if __name__=='__main__':
+  app.run(debug=True)
